aea11e4a440fa5990adb7ba2ea25b7e034fa5f38
bluesky0318 <42303596@qq.com>
2014-11-22 18:15:39 +0800

    Successful to build refinery cms system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = '9d7f099441e9d8f6148d97260aa63d93e32b9a3453e9b06a8bcc4d9dc4c1ea6eeb13319e415987946c1c6a3c6d0a6f2c092d8b59413f850f8936fe421db510d5'
